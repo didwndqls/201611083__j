889088416fad79d2fd3e29c7d1860d6ee00d5185
didwndqls <didwndqls@naver.com>
2016-09-19 16:27:00 +0900

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,14 @@
+﻿class Hello {
+String name;
+Hello(String name){
+this.name=name;
+}
+void sayHello() {
+System.out.println("Hello"+name);
+}
+}
+
+Hello h = new Hello("world");
+
+
+h.sayHello();
