8fde1e740f9f24616542256151eb3b0b986abb32
201611083__j  양중빈 <didwndqls@naver.com>
2016-10-06 23:01:10 +0900

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,17 @@
+﻿class Player {
+    private String name=null;
+    private int num=0;
+    public Player(String name) {
+        this.name=name;
+    }
+    public void play() {
+        Dice d=new Dice();
+        num=d.roll();
+    }
+    public int getNum() {
+	return this.num;
+    }
+    public String getName() {
+	return this.name;
+    }
+}
